493a3e0503b71702109f007d06744dc32bb851e6
chshouyu <chshouyu@126.com>
2020-10-18 21:40:38 +0800

    config eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:node/recommended',
+    'plugin:promise/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: [],
+  parserOptions: {
+    project: path.resolve(__dirname, './tsconfig.eslint.json'),
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  settings: {},
+  globals: {},
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-console': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'node/no-missing-import': [
+      'error',
+      {
+        tryExtensions: ['.ts', '.js', '.json']
+      }
+    ]
+  }
+};
